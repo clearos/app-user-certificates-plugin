24b220fe6a96b33dd7f3ceb7972fe8d1d1b4b945
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-03-28 11:39:05 +0000

    - Prep 1.0.10 snapshot

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'user_certificates_plugin';
-$app['version'] = '1.0.9';
+$app['version'] = '1.0.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-user-certificates-plugin.spec
+++ packaging/app-user-certificates-plugin.spec
@@ -1,7 +1,7 @@
 
 Name: app-user-certificates-plugin
 Epoch: 1
-Version: 1.0.9
+Version: 1.0.10
 Release: 1%{dist}
 Summary: Security Certificates Policies - APIs and install
 License: LGPLv3
