5081c2cdd1897c4cf9b54360a760a5f39ab86c06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:02:59 +0000

    Add new files and directories and commit changes to local repository

--- language/tg/user_certificates_plugin_lang.php
+++ language/tg/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'Сертификати корбар сиёсат таъмини идоракунии дастрасӣ, барои шаҳодатномаҳои амниятӣ аст.';
+$lang['user_certificates_plugin_app_name'] = 'Шаҳодатномаҳои амниятӣ сиёсат';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Шаҳодатномаҳои амниятӣ Корбар';
 
