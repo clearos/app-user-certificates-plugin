1ca1f4ceb23a3331ed4e5807af2e16b654a0d7b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:04:06 +0000

    Add new files and directories and commit changes to local repository

--- language/vi/user_certificates_plugin_lang.php
+++ language/vi/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'Người sử dụng Giấy chứng nhận Chính sách cung cấp kiểm soát truy cập cho các chứng chỉ bảo mật.';
+$lang['user_certificates_plugin_app_name'] = 'Giấy chứng nhận an ninh Chính sách';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Giấy chứng nhận an ninh tài';
 
