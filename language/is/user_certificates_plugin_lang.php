49468b5032ff35d220dba7c38b9502fe08a2660a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:32:28 +0000

    Add new files and directories and commit changes to local repository

--- language/is/user_certificates_plugin_lang.php
+++ language/is/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'User certificates Reglur veita aðgangsstýringu fyrir öryggi vottorð.';
+$lang['user_certificates_plugin_app_name'] = 'Öryggisvottorð Reglur';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Öryggi Vottorð User';
 
