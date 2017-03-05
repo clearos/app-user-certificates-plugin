80d888d9941b68174ac57d4a0cc38d9798fe5ff3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:32:19 +0000

    Add new files and directories and commit changes to local repository

--- language/ig/user_certificates_plugin_lang.php
+++ language/ig/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'User Asambodo atumatu inye ohere akara maka nche asambodo.';
+$lang['user_certificates_plugin_app_name'] = 'Security Asambodo atumatu';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Security Asambodo User';
 
