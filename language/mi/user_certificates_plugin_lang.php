6d7952f0ed86fb90f1fdfceb38ffb96b9d7a4cc8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:34:19 +0000

    Add new files and directories and commit changes to local repository

--- language/mi/user_certificates_plugin_lang.php
+++ language/mi/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'whakarato Tiwhikete Kaiwhakamahi kaupapa here mana uru mo nga tiwhikete haumarutanga.';
+$lang['user_certificates_plugin_app_name'] = 'Tiwhikete Haumarutanga kaupapa here';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Kaiwhakamahi Haumarutanga Tiwhikete';
 
