62e48ff37d63827d3f51c3ca5224a5b712328045
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:38:53 +0000

    Add new files and directories and commit changes to local repository

--- language/zu/user_certificates_plugin_lang.php
+++ language/zu/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'Umsebenzisi Izitifiketi Izinqubomgomo ukunikeza control ukufinyelela izitifiketi zokuphepha.';
+$lang['user_certificates_plugin_app_name'] = 'Security Izitifiketi Izinqubomgomo';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Security Izitifiketi Umsebenzisi';
 
