6010d924a367307131cde29015ef06eeca17500c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:36:47 +0000

    Add new files and directories and commit changes to local repository

--- language/so/user_certificates_plugin_lang.php
+++ language/so/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['user_certificates_plugin_app_description'] = 'User Certificates Siyaasadaha bixiyaan helitaanka gacanta for shahaadooyin ammaanka.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
+$lang['user_certificates_plugin_app_name'] = 'Certificates Security Policies';
 $lang['user_certificates_plugin_pptp_server_user'] = 'Certificates Security User';
 
