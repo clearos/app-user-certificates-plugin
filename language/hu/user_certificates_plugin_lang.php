b64d526ef6e41a2321dd6346e8e2d5240b4a5422
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:31:55 +0000

    Add new files and directories and commit changes to local repository

--- language/hu/user_certificates_plugin_lang.php
+++ language/hu/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'Felhasználói tanúsítványok házirendje eléréskorlátozást a biztonsági tanúsítványokat.';
+$lang['user_certificates_plugin_app_name'] = 'Biztonsági tanúsítványok házirendje';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Biztonsági tanúsítványok Felhasználó';
 
