eccbc961a31f873aba78c8443741613e6881493f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:33:10 +0000

    Add new files and directories and commit changes to local repository

--- language/km/user_certificates_plugin_lang.php
+++ language/km/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'គោលនយោបាយវិញ្ញាបនបត្រអ្នកប្រើផ្ដល់នូវការត្រួតពិនិត្យការចូលដំណើរការសម្រាប់វិញ្ញាបនបត្រសន្តិសុខ។';
+$lang['user_certificates_plugin_app_name'] = 'គោលនយោបាយវិញ្ញាបនបត្រសន្តិសុខ';
+$lang['user_certificates_plugin_pptp_server_user'] = 'អ្នកប្រើវិញ្ញាបនបត្រសន្តិសុខ';
 
