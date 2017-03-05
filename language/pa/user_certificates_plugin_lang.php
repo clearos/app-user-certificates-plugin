c41ea03a0a86f62b40869dafd18610e61b4f8604
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:35:46 +0000

    Add new files and directories and commit changes to local repository

--- language/pa/user_certificates_plugin_lang.php
+++ language/pa/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['user_certificates_plugin_app_description'] = 'ਯੂਜ਼ਰ ਸਰਟੀਫਿਕੇਟ ਡਰਾਇਰ ਸੁਰੱਖਿਆ ਨੂੰ ਸਰਟੀਫਿਕੇਟ ਲਈ ਪਹੁੰਚ ਕੰਟਰੋਲ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_name'] = 'ਸੁਰੱਖਿਆ ਸਰਟੀਫਿਕੇਟ ਡਰਾਇਰ';
+$lang['user_certificates_plugin_pptp_server_user'] = 'ਸੁਰੱਖਿਆ ਸਰਟੀਫਿਕੇਟ ਯੂਜ਼ਰ';
 
