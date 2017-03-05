c570cb30b571639abef23c130f85861210889add
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:36:56 +0000

    Add new files and directories and commit changes to local repository

--- language/st/user_certificates_plugin_lang.php
+++ language/st/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['user_certificates_plugin_app_description'] = 'User mangolo Maano fana phihlello ya taolo ya bakeng sa disetifikeiti ea tšireletseho.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
+$lang['user_certificates_plugin_app_name'] = 'Disetifikeiti Security Maano';
 $lang['user_certificates_plugin_pptp_server_user'] = 'Security mangolo User';
 
