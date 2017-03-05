eab5e55f1dc7d9576e186b152a3f8132fc943c9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:32:11 +0000

    Add new files and directories and commit changes to local repository

--- language/ga/user_certificates_plugin_lang.php
+++ language/ga/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'a chur ar fáil Deimhnithe Úsáideoir Polasaithe rialaithe rochtana do na deimhnithe slándála.';
+$lang['user_certificates_plugin_app_name'] = 'Teastais Slándáil Polasaithe';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Teastais Slándáil Úsáideora';
 
