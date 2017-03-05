b0a02def1e767486d11a3e920581668855b17b1b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:28:17 +0000

    Add new files and directories and commit changes to local repository

--- language/hy/user_certificates_plugin_lang.php
+++ language/hy/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'Օգտագործողի վկայականների քաղաքականությունը տրամադրել մուտքի վերահսկման անվտանգության համար հավաստագրեր:';
+$lang['user_certificates_plugin_app_name'] = 'Անվտանգության վկայագրեր քաղաքականությունը';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Անվտանգության վկայագրեր Օգտվող';
 
