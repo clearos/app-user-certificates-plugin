b01d4cd43c0e73a29d3be920d3ccd7ce95dc7be4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:37:19 +0000

    Add new files and directories and commit changes to local repository

--- language/sw/user_certificates_plugin_lang.php
+++ language/sw/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
+$lang['user_certificates_plugin_app_description'] = 'User Vyeti Sera kutoa upatikanaji wa kudhibiti kwa vyeti vya usalama.';
 $lang['user_certificates_plugin_app_name'] = 'Vyeti Security Sera';
 $lang['user_certificates_plugin_pptp_server_user'] = 'Usalama Vyeti mtumiaji';
 
