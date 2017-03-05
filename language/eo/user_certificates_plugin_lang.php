7d12911436c51e65396de661de2773ad8ca37111
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:30:18 +0000

    Add new files and directories and commit changes to local repository

--- language/eo/user_certificates_plugin_lang.php
+++ language/eo/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'Uzanto registritaj Politikoj havigi aliron kontrolo por la sekureco atestilojn.';
+$lang['user_certificates_plugin_app_name'] = 'Sekureco registritaj Politikoj';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Sekureco registritaj Uzanto';
 
