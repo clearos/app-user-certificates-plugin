5346c90195dce6067389bf4d37264ffc7c01315d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:29:32 +0000

    Add new files and directories and commit changes to local repository

--- language/ny/user_certificates_plugin_lang.php
+++ language/ny/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'Wosuta Zikalata Ndondomeko imapatsa kulamulira kwa zikalata chitetezo.';
+$lang['user_certificates_plugin_app_name'] = 'Zikalata Security Ndondomeko';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Security Zikalata Wosuta';
 
