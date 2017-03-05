3edba3fc3f855602f67df91654c1a965c064e272
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:30:36 +0000

    Add new files and directories and commit changes to local repository

--- language/fi/user_certificates_plugin_lang.php
+++ language/fi/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'Käyttäjän Sertifikaatit säännöt tarjoavat kulunvalvonta että varmenteita.';
+$lang['user_certificates_plugin_app_name'] = 'Turvallisuus Sertifikaatit säännöt';
+$lang['user_certificates_plugin_pptp_server_user'] = 'Turvallisuus Sertifikaatit Käyttäjä';
 
