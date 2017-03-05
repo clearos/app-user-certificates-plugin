0d658389d9d62015089e4650f094007135c86f30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 02:34:54 +0000

    Add new files and directories and commit changes to local repository

--- language/mr/user_certificates_plugin_lang.php
+++ language/mr/user_certificates_plugin_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['user_certificates_plugin_app_description'] = 'User Certificates Policies provide access control for the security certificates.';
-$lang['user_certificates_plugin_app_name'] = 'Security Certificates Policies';
-$lang['user_certificates_plugin_pptp_server_user'] = 'Security Certificates User';
+$lang['user_certificates_plugin_app_description'] = 'वापरकर्ता प्रमाणपत्र धोरणे सुरक्षितता प्रमाणपत्रे प्रवेश नियंत्रण प्रदान.';
+$lang['user_certificates_plugin_app_name'] = 'सुरक्षा प्रमाणपत्र धोरणे';
+$lang['user_certificates_plugin_pptp_server_user'] = 'सुरक्षा प्रमाणपत्र वापरकर्ता';
 
